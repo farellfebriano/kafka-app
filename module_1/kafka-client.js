6e6744816af6189b759b70ee483ed44e038a437c
farellfebriano <farellfebriano8@gmail.com>
2024-12-17 20:06:31 -0600

    first attemp

---
+++ module_1/kafka-client.js
@@ -0,0 +1,22 @@
+const kafka = require("kafka-node");
+// const client = new kafka.KafkaClient({ kafkaHost: "kafka:9092" });
+const client = new kafka.KafkaClient({ kafkaHost: "192.168.254.182:9094" });
+const Producer = kafka.Producer;
+const producer = new Producer(client);
+
+// message
+const payloads = [
+  { topic: "test", messages: "hi, im 192.168.254.182", partition: 0 },
+  { topic: "test", messages: "hi, im 192.168.254.182", partition: 0 },
+];
+
+producer.on("ready", function () {
+  console.log("sending payload");
+  producer.send(payloads, function (err, data) {
+    console.log(data);
+  });
+});
+
+producer.on("error", function (err) {
+  console.log(err);
+});
